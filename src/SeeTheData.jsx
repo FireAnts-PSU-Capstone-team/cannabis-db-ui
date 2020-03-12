039c42f8f50a42a7c0758ca30b6412e5f42a16d4
Ha Ly <lyha@pdx.edu>
2020-03-11 17:12:21 -0700

    Implement main data table page with fake data

--- src/SeeTheData.jsx
+++ src/SeeTheData.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import DataTable from './DataTable';
 
 export default function SeeTheData() {
   return (
-    <div>
-      <h1>See the data</h1>
-    </div>
+    <DataTable />
   );
 }
