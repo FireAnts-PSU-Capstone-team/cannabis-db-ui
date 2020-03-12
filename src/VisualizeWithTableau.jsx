59ddb7091f0e2eb2a4691c0c497ecee6cf8435d4
Ha Ly <lyha@pdx.edu>
2020-03-11 17:14:20 -0700

    Add fake content to tableau page

--- src/VisualizeWithTableau.jsx
+++ src/VisualizeWithTableau.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
 export default function VisualizeWithTableau() {
   return (
-    <div>
-      <h1>Visualize with Tableau</h1>
-    </div>
+    <Container maxWidth="lg">
+      <iframe title="youtube" width="100%" height="500" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <Button style={{ marginRight: '1rem' }} variant="outlined" color="primary">
+        Download this workbook
+      </Button>
+      <Button variant="contained" color="primary">
+        Open in Tableau Desktop
+      </Button>
+    </Container>
   );
 }
