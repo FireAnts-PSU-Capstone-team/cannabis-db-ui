334810d94b767bcc30f802aeaa0cbdc01539d5e7
Ha Ly <lyha@pdx.edu>
2020-05-11 16:02:45 -0700

    Updates

---
+++ precache-manifest.f3dbfc433c5954871ae318fafd454873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc02862a99c2b5753aad75af475f4f1",
+    "url": "/cannabis-db-ui/index.html"
+  },
+  {
+    "revision": "2a3d678c2b892c041f61",
+    "url": "/cannabis-db-ui/static/css/main.4813387a.chunk.css"
+  },
+  {
+    "revision": "6ff1f730a187d487daf6",
+    "url": "/cannabis-db-ui/static/js/2.83ee5aba.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/cannabis-db-ui/static/js/2.83ee5aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3d678c2b892c041f61",
+    "url": "/cannabis-db-ui/static/js/main.751463e0.chunk.js"
+  },
+  {
+    "revision": "eb06e53a1e2a95aac7e3",
+    "url": "/cannabis-db-ui/static/js/runtime-main.36a161ac.js"
+  }
+]);
