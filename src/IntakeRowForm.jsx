e587844cb7442f23be20a17fbc1b28f46ab0354b
Ha Ly <lyha@pdx.edu>
2020-05-11 17:33:40 -0700

    Fix json fields to match real api

--- src/IntakeRowForm.jsx
+++ src/IntakeRowForm.jsx
@@ -46,63 +46,63 @@ export default function IntakeRowForm(props) {
     "Notes": ""
   };
 
-  const [rowValue, setRowValue] = useState(theRow['row']);
-  const [submissionDateValue, setSubmissionDateValue] = useState(theRow['Submission date']);
-  const [entityValue, setEntityValue] = useState(theRow['Entity']);
-  const [dbaValue, setDbaValue] = useState(theRow['DBA']);
-  const [facilityAddressValue, setFacilityAddressValue] = useState(theRow['Facility Address']);
-  const [facilitySuiteNoValue, setFacilitySuiteNoValue] = useState(theRow['Facility Suite #']);
-  const [facilityZipValue, setFacilityZipValue] = useState(theRow['Facility Zip']);
-  const [mailingAddressValue, setMailingAddressValue] = useState(theRow['Mailing Address']);
-  const [mrlValue, setMrlValue] = useState(theRow['MRL']);
-  const [neighborhoodAssocValue, setneighborhoodAssocValue] = useState(theRow['Neighborhood Association']);
-  const [complianceRegionValue, setComplianceRegionValue] = useState(theRow['Compliance Region']);
-  const [firstNameValue, setFirstNameValue] = useState(theRow['Primary Contact First Name']);
-  const [lastNameValue, setLastNameValue] = useState(theRow['Primary Contact Last Name']);
-  const [emailValue, setEmailValue] = useState(theRow['Email']);
-  const [phoneValue, setPhoneValue] = useState(theRow['Phone']);
-  const [endorseTypeValue, setEndorseTypeValue] = useState(theRow['Endorse Type']);
-  const [licenseTypeValue, setLicenseTypeValue] = useState(theRow['License Type']);
-  const [repeatLocationValue, setRepeatLocationValue] = useState(theRow['Repeat location?']);
-  const [appCompleteValue, setAppCompleteValue] = useState(theRow['App complete?']);
-  const [feeScheduleValue, setFeeScheduleValue] = useState(theRow['Fee Schedule']);
-  const [receiptNoValue, setReceiptNoValue] = useState(theRow['Receipt No.']);
-  const [cashAmountValue, setCashAmountValue] = useState(theRow['Cash Amount']);
-  const [checkAmountValue, setCheckAmountValue] = useState(theRow['Check Amount']);
-  const [cardAmountValue, setCardAmountValue] = useState(theRow['Card Amount']);
-  const [checkNoValue, setCheckNoValue] = useState(theRow['Check No. / Approval Code']);
-  const [mrlNoValue, setMrlNoValue] = useState(theRow['MRL#']);
-  const [notesValue, setNotesValue] = useState(theRow['Notes']);
+  const [rowValue, setRowValue] = useState(theRow.row);
+  const [submissionDateValue, setSubmissionDateValue] = useState(theRow.submission_date);
+  const [entityValue, setEntityValue] = useState(theRow.entity);
+  const [dbaValue, setDbaValue] = useState(theRow.dba);
+  const [facilityAddressValue, setFacilityAddressValue] = useState(theRow.facility_address);
+  const [facilitySuiteNoValue, setFacilitySuiteNoValue] = useState(theRow.facility_suite);
+  const [facilityZipValue, setFacilityZipValue] = useState(theRow.facility_zip);
+  const [mailingAddressValue, setMailingAddressValue] = useState(theRow.mailing_address);
+  const [mrlValue, setMrlValue] = useState(theRow.mrl);
+  const [neighborhoodAssocValue, setneighborhoodAssocValue] = useState(theRow.neighborhood_association);
+  const [complianceRegionValue, setComplianceRegionValue] = useState(theRow.compliance_region);
+  const [firstNameValue, setFirstNameValue] = useState(theRow.primary_contact_first_name);
+  const [lastNameValue, setLastNameValue] = useState(theRow.primary_contact_last_name);
+  const [emailValue, setEmailValue] = useState(theRow.email);
+  const [phoneValue, setPhoneValue] = useState(theRow.phone);
+  const [endorseTypeValue, setEndorseTypeValue] = useState(theRow.endorse_type);
+  const [licenseTypeValue, setLicenseTypeValue] = useState(theRow.license_type);
+  const [repeatLocationValue, setRepeatLocationValue] = useState(theRow.repeat_location);
+  const [appCompleteValue, setAppCompleteValue] = useState(theRow.app_complete);
+  const [feeScheduleValue, setFeeScheduleValue] = useState(theRow.fee_schedule);
+  const [receiptNoValue, setReceiptNoValue] = useState(theRow.receipt_num);
+  const [cashAmountValue, setCashAmountValue] = useState(theRow.cash_amount);
+  const [checkAmountValue, setCheckAmountValue] = useState(theRow.check_amount);
+  const [cardAmountValue, setCardAmountValue] = useState(theRow.card_amount);
+  const [checkNoValue, setCheckNoValue] = useState(theRow.check_num_approval_code);
+  const [mrlNoValue, setMrlNoValue] = useState(theRow.mrl_num);
+  const [notesValue, setNotesValue] = useState(theRow.notes);
 
   // Update states every time the dialog opens
   useEffect(() => {
-    setRowValue(theRow['row']);
-    setSubmissionDateValue(theRow['Submission date']);
-    setEntityValue(theRow['Entity']);
-    setDbaValue(theRow['DBA']);
-    setFacilityAddressValue(theRow['Facility Address']);
-    setFacilitySuiteNoValue(theRow['Facility Suite #']);
-    setFacilityZipValue(theRow['Facility Zip']);
-    setMailingAddressValue(theRow['Mailing Address']);
-    setMrlValue(theRow['MRL']);
-    setneighborhoodAssocValue(theRow['Neighborhood Association']);
-    setComplianceRegionValue(theRow['Compliance Region']);
-    setFirstNameValue(theRow['Primary Contact First Name']);
-    setLastNameValue(theRow['Primary Contact Last Name']);
-    setEmailValue(theRow['Email']);
-    setPhoneValue(theRow['Phone']);
-    setEndorseTypeValue(theRow['Endorse Type']);
-    setLicenseTypeValue(theRow['License Type']);
-    setRepeatLocationValue(theRow['Repeat location?']);
-    setAppCompleteValue(theRow['App complete?']);
-    setFeeScheduleValue(theRow['Fee Schedule']);
-    setReceiptNoValue(theRow['Receipt No.']);
-    setCashAmountValue(theRow['Cash Amount']);
-    setCheckAmountValue(theRow['Check Amount']);
-    setCardAmountValue(theRow['Card Amount']);
-    setCheckNoValue(theRow['Check No. / Approval Code']);
-    setMrlNoValue(theRow['MRL#']);
-    setNotesValue(theRow['Notes']);
+    setRowValue(theRow.row);
+    setSubmissionDateValue(theRow.submission_date);
+    setEntityValue(theRow.entity);
+    setDbaValue(theRow.dba);
+    setFacilityAddressValue(theRow.facility_address);
+    setFacilitySuiteNoValue(theRow.facility_suite);
+    setFacilityZipValue(theRow.facility_zip);
+    setMailingAddressValue(theRow.mailing_address);
+    setMrlValue(theRow.mrl);
+    setneighborhoodAssocValue(theRow.neighborhood_association);
+    setComplianceRegionValue(theRow.compliance_region);
+    setFirstNameValue(theRow.primary_contact_first_name);
+    setLastNameValue(theRow.primary_contact_last_name);
+    setEmailValue(theRow.email);
+    setPhoneValue(theRow.phone);
+    setEndorseTypeValue(theRow.endorse_type);
+    setLicenseTypeValue(theRow.license_type);
+    setRepeatLocationValue(theRow.repeat_location);
+    setAppCompleteValue(theRow.app_complete);
+    setFeeScheduleValue(theRow.fee_schedule);
+    setReceiptNoValue(theRow.receipt_num);
+    setCashAmountValue(theRow.cash_amount);
+    setCheckAmountValue(theRow.check_amount);
+    setCardAmountValue(theRow.card_amount);
+    setCheckNoValue(theRow.check_num_approval_code);
+    setMrlNoValue(theRow.mrl_num);
+    setNotesValue(theRow.notes);
   }, [isDialog, dialogOpen, theRow]);
 
   const makeRowToSubmit = () => {
@@ -154,48 +154,39 @@ export default function IntakeRowForm(props) {
 
   const formFields = () =>
     <>
-      {makeField('row', theRow['row'], e => setRowValue(e.target.value))}
-      <TextField
-        style={{ marginBottom: '1rem' }}
-        fullWidth
-        label="Submission date"
-        variant="outlined"
-        type="date"
-        InputLabelProps={{ shrink: true }}
-        onChange={e => setSubmissionDateValue(formatDate(e.target.value))}
-        defaultValue={reverseFormatDate(theRow['Submission date'])}
-      />
-      {makeField('Entity', theRow['Entity'], e => setEntityValue(e.target.value))}
-      {makeField('DBA', theRow['DBA'], e => setDbaValue(e.target.value))}
-      {makeField('Facility Address', theRow['Facility Address'], e => setFacilityAddressValue(e.target.value))}
-      {makeField('Facility Suite #', theRow['Facility Suite #'], e => setFacilitySuiteNoValue(e.target.value))}
-      {makeField('Facility Zip', theRow['Facility Zip'], e => setFacilityZipValue(e.target.value))}
-      {makeField('Mailing Address', theRow['Mailing Address'], e => setMailingAddressValue(e.target.value))}
-      {makeField('MRL', theRow['MRL'], e => setMrlValue(e.target.value))}
-      {makeField('Neighborhood Association', theRow['Neighborhood Association'], e => setneighborhoodAssocValue(e.target.value))}
-      {makeField('Compliance Region', theRow['Compliance Region'], e => setComplianceRegionValue(e.target.value))}
-      {makeField('Primary Contact First Name', theRow['Primary Contact First Name'], e => setFirstNameValue(e.target.value))}
-      {makeField('Primary Contact Last Name', theRow['Primary Contact Last Name'], e => setLastNameValue(e.target.value))}
-      {makeField('Email', theRow['Email'], e => setEmailValue(e.target.value))}
-      {makeField('Phone', theRow['Phone'], e => setPhoneValue(e.target.value))}
-      {makeField('Endorse Type', theRow['Endorse Type'], e => setEndorseTypeValue(e.target.value))}
-      {makeField('License Type', theRow['License Type'], e => setLicenseTypeValue(e.target.value))}
-      {makeField('Repeat location?', theRow['Repeat location?'], e => setRepeatLocationValue(e.target.value))}
-      {makeField('App complete?', theRow['App complete?'], e => setAppCompleteValue(e.target.value))}
-      {makeField('Fee Schedule', theRow['Fee Schedule'], e => setFeeScheduleValue(e.target.value))}
-      {makeField('Receipt No.', theRow['Receipt No.'], e => setReceiptNoValue(e.target.value))}
-      {makeField('Cash Amount', theRow['Cash Amount'], e => setCashAmountValue(e.target.value))}
-      {makeField('Check Amount', theRow['Check Amount'], e => setCheckAmountValue(e.target.value))}
-      {makeField('Card Amount', theRow['Card Amount'], e => setCardAmountValue(e.target.value))}
-      {makeField('Check No. / Approval Code', theRow['Check No. / Approval Code'], e => setCheckNoValue(e.target.value))}
-      {makeField('MRL#', theRow['MRL#'], e => setMrlNoValue(e.target.value))}
-      {makeField('Notes', theRow['Notes'], e => setNotesValue(e.target.value))}
+      {makeField('row', theRow.row, e => setRowValue(e.target.value))}
+      {makeField('Submission date', theRow.submission_date, e => setSubmissionDateValue(e.target.value))}
+      {makeField('Entity', theRow.entity, e => setEntityValue(e.target.value))}
+      {makeField('DBA', theRow.dba, e => setDbaValue(e.target.value))}
+      {makeField('Facility Address', theRow.facility_address, e => setFacilityAddressValue(e.target.value))}
+      {makeField('Facility Suite #', theRow.facility_suite, e => setFacilitySuiteNoValue(e.target.value))}
+      {makeField('Facility Zip', theRow.facility_zip, e => setFacilityZipValue(e.target.value))}
+      {makeField('Mailing Address', theRow.mailing_address, e => setMailingAddressValue(e.target.value))}
+      {makeField('MRL', theRow.mrl, e => setMrlValue(e.target.value))}
+      {makeField('Neighborhood Association', theRow.neighborhood_association, e => setneighborhoodAssocValue(e.target.value))}
+      {makeField('Compliance Region', theRow.compliance_region, e => setComplianceRegionValue(e.target.value))}
+      {makeField('Primary Contact First Name', theRow.primary_contact_first_name, e => setFirstNameValue(e.target.value))}
+      {makeField('Primary Contact Last Name', theRow.primary_contact_last_name, e => setLastNameValue(e.target.value))}
+      {makeField('Email', theRow.email, e => setEmailValue(e.target.value))}
+      {makeField('Phone', theRow.phone, e => setPhoneValue(e.target.value))}
+      {makeField('Endorse Type', theRow.endorse_type, e => setEndorseTypeValue(e.target.value))}
+      {makeField('License Type', theRow.license_type, e => setLicenseTypeValue(e.target.value))}
+      {makeField('Repeat location?', theRow.repeat_location, e => setRepeatLocationValue(e.target.value))}
+      {makeField('App complete?', theRow.app_complete, e => setAppCompleteValue(e.target.value))}
+      {makeField('Fee Schedule', theRow.fee_schedule, e => setFeeScheduleValue(e.target.value))}
+      {makeField('Receipt No.', theRow.receipt_num, e => setReceiptNoValue(e.target.value))}
+      {makeField('Cash Amount', theRow.cash_amount, e => setCashAmountValue(e.target.value))}
+      {makeField('Check Amount', theRow.check_amount, e => setCheckAmountValue(e.target.value))}
+      {makeField('Card Amount', theRow.card_amount, e => setCardAmountValue(e.target.value))}
+      {makeField('Check No. / Approval Code', theRow.check_num_approval_code, e => setCheckNoValue(e.target.value))}
+      {makeField('MRL#', theRow.mrl_num, e => setMrlNoValue(e.target.value))}
+      {makeField('Notes', theRow.notes, e => setNotesValue(e.target.value))}
     </>;
 
   if (isDialog === true) {
     return (
       <Dialog open={dialogOpen} onClose={onDialogClose} aria-labelledby="intake-row-form-dialog">
-        <DialogTitle>Edit row {theRow['row']}</DialogTitle>
+        <DialogTitle>Edit row {theRow.row}</DialogTitle>
         <DialogContent>
           {formFields()}
         </DialogContent>
--- src/IntakeTableRow.jsx
+++ src/IntakeTableRow.jsx
@@ -23,33 +23,33 @@ export default function IntakeTableRow(props) {
           inputProps={{ 'aria-labelledby': labelId }}
         />
       </TableCell>
-      <TableCell key={makeCellKey('row')} align="right">{row['row']}</TableCell>
-      <TableCell key={makeCellKey('Submission date')} align="left">{row['Submission date']}</TableCell>
-      <TableCell key={makeCellKey('Entity')} align="left">{row['Entity']}</TableCell>
-      <TableCell key={makeCellKey('DBA')} align="left">{row['DBA']}</TableCell>
-      <TableCell key={makeCellKey('Facility Address')} align="left">{row['Facility Address']}</TableCell>
-      <TableCell key={makeCellKey('Facility Suite #')} align="right">{row['Facility Suite #']}</TableCell>
-      <TableCell key={makeCellKey('Facility Zip')} align="right">{row['Facility Zip']}</TableCell>
-      <TableCell key={makeCellKey('Mailing Address')} align="left">{row['Mailing Address']}</TableCell>
-      <TableCell key={makeCellKey('MRL')} align="left">{row['MRL']}</TableCell>
-      <TableCell key={makeCellKey('Neighborhood Association')} align="left">{row['Neighborhood Association']}</TableCell>
-      <TableCell key={makeCellKey('Compliance Region')} align="left">{row['Compliance Region']}</TableCell>
-      <TableCell key={makeCellKey('Primary Contact First Name')} align="left">{row['Primary Contact First Name']}</TableCell>
-      <TableCell key={makeCellKey('Primary Contact Last Name')} align="left">{row['Primary Contact Last Name']}</TableCell>
-      <TableCell key={makeCellKey('Email')} align="left">{row['Email']}</TableCell>
-      <TableCell key={makeCellKey('Phone')} align="left">{row['Phone']}</TableCell>
-      <TableCell key={makeCellKey('Endorse Type')} align="left">{row['Endorse Type']}</TableCell>
-      <TableCell key={makeCellKey('License Type')} align="left">{row['License Type']}</TableCell>
-      <TableCell key={makeCellKey('Repeat location?')} align="left">{row['Repeat location?']}</TableCell>
-      <TableCell key={makeCellKey('App complete?')} align="left">{row['App complete?']}</TableCell>
-      <TableCell key={makeCellKey('Fee Schedule')} align="left">{row['Fee Schedule']}</TableCell>
-      <TableCell key={makeCellKey('Receipt No.')} align="left">{row['Receipt No.']}</TableCell>
-      <TableCell key={makeCellKey('Cash Amount')} align="left">{row['Cash Amount']}</TableCell>
-      <TableCell key={makeCellKey('Check Amount')} align="left">{row['Check Amount']}</TableCell>
-      <TableCell key={makeCellKey('Card Amount')} align="left">{row['Card Amount']}</TableCell>
-      <TableCell key={makeCellKey('Check No. / Approval Code')} align="right">{row['Check No. / Approval Code']}</TableCell>
-      <TableCell key={makeCellKey('MRL#')} align="left">{row['MRL#']}</TableCell>
-      <TableCell key={makeCellKey('Notes')} align="left">{row['Notes']}</TableCell>
+      <TableCell key={makeCellKey('row')} align="right">{row.row}</TableCell>
+      <TableCell key={makeCellKey('Submission date')} align="left">{row.submission_date}</TableCell>
+      <TableCell key={makeCellKey('Entity')} align="left">{row.entity}</TableCell>
+      <TableCell key={makeCellKey('DBA')} align="left">{row.dba}</TableCell>
+      <TableCell key={makeCellKey('Facility Address')} align="left">{row.facility_address}</TableCell>
+      <TableCell key={makeCellKey('Facility Suite #')} align="right">{row.facility_suite}</TableCell>
+      <TableCell key={makeCellKey('Facility Zip')} align="right">{row.facility_zip}</TableCell>
+      <TableCell key={makeCellKey('Mailing Address')} align="left">{row.mailing_address}</TableCell>
+      <TableCell key={makeCellKey('MRL')} align="left">{row.mrl}</TableCell>
+      <TableCell key={makeCellKey('Neighborhood Association')} align="left">{row.neighborhood_association}</TableCell>
+      <TableCell key={makeCellKey('Compliance Region')} align="left">{row.compliance_region}</TableCell>
+      <TableCell key={makeCellKey('Primary Contact First Name')} align="left">{row.primary_contact_first_name}</TableCell>
+      <TableCell key={makeCellKey('Primary Contact Last Name')} align="left">{row.primary_contact_last_name}</TableCell>
+      <TableCell key={makeCellKey('Email')} align="left">{row.email}</TableCell>
+      <TableCell key={makeCellKey('Phone')} align="left">{row.phone}</TableCell>
+      <TableCell key={makeCellKey('Endorse Type')} align="left">{row.endorse_type}</TableCell>
+      <TableCell key={makeCellKey('License Type')} align="left">{row.license_type}</TableCell>
+      <TableCell key={makeCellKey('Repeat location?')} align="left">{row.repeat_location}</TableCell>
+      <TableCell key={makeCellKey('App complete?')} align="left">{row.app_complete}</TableCell>
+      <TableCell key={makeCellKey('Fee Schedule')} align="left">{row.fee_schedule}</TableCell>
+      <TableCell key={makeCellKey('Receipt No.')} align="left">{row.receipt_num}</TableCell>
+      <TableCell key={makeCellKey('Cash Amount')} align="left">{row.cash_amount}</TableCell>
+      <TableCell key={makeCellKey('Check Amount')} align="left">{row.check_amount}</TableCell>
+      <TableCell key={makeCellKey('Card Amount')} align="left">{row.card_amount}</TableCell>
+      <TableCell key={makeCellKey('Check No. / Approval Code')} align="right">{row.check_num_approval_code}</TableCell>
+      <TableCell key={makeCellKey('MRL#')} align="left">{row.mrl_num}</TableCell>
+      <TableCell key={makeCellKey('Notes')} align="left">{row.notes}</TableCell>
     </TableRow>
   );
 }
